060627c03fdd7e595be8947ebef17c816c82edf7
Stathis Sideris <sideris@gmail.com>
2011-08-25 19:55:32 +0100

    Basic video player gui

--- src/film_tempo/gui.clj
+++ src/film_tempo/gui.clj
@@ -1,28 +1,40 @@
-(ns film-tempo
+(ns film-tempo.gui
   (:require [clojure.contrib.miglayout :as mig]
             [clarity.component :as c]
             clarity.utils))
 
-(defn make-player-panel []
-  (let [columns 5]
-    (mig/miglayout (c/make :panel)
-                   :layout [:wrap columns]
-                   :column "[grow,fill][grow,fill][grow,fill][grow,fill][grow,fill]"
-                   ;;video area
-                   (c/make :label "Film tempo v0.1" [:id :header]) [:span columns] 
-                   (c/make :panel [:id :video-area]) [:span columns]
-                   ;;controls
-                   (mig/miglayout (c/make :panel)
-                                  :column "3[]3[grow,fill]3[]3"
-                                  (c/make :label "00:00" [:id :current-time-label])
-                                  (c/make :slider [:id :position-slider])
-                                  (c/make :label "00:00" [:id :total-time-label]))
-                   [:span columns]
-                   (c/make :button "[]" [:id :stop-button])
-                   (c/make :button ">" [:id :play-button])
-                   (c/make :button "cut" [:id :cut-button])
-                   (c/make :button "x1" [:id :normal-speed-button])
-                   (c/make :button "x0.5" [:id :half-speed-button])
-                   (c/make :panel [:id :timeline-area]) [:span columns])))
+(defn image-panel [image]
+  (let [p (proxy [javax.swing.JPanel] []
+            (paintComponent [g] (if @image (.drawImage g @image 0 0 this)))
+            (getPreferredSize[] (if @image (java.awt.Dimension.
+                                            (.getWidth @image)
+                                            (.getHeight @image))
+                                    (java.awt.Dimension. 100 100))))]
+    (add-watch image "image-watch" (fn [k r o n] (.repaint p)))
+    p))
 
-(clarity.utils/show-comp (make-player-panel))
+(defn make-player-panel [image]
+  (mig/miglayout (c/make :panel)
+                 :layout [:wrap 5]
+                 :column "[grow,fill][grow,fill][grow,fill][grow,fill][grow,fill]"
+                 ;;video area
+                 (c/make :label "Film tempo v0.1" [:id :header]) :span
+                 (image-panel image) :span
+                 ;;controls
+                 (mig/miglayout (c/make :panel)
+                                :column "3[]3[grow,fill]3[]3"
+                                (c/make :label "00:00" [:id :current-time-label])
+                                (c/make :slider [:id :position-slider])
+                                (c/make :label "00:00" [:id :total-time-label]))
+                 :span
+                 (c/make :button "[]" [:id :stop-button])
+                 (c/make :button ">" [:id :play-button])
+                 (c/make :button "cut" [:id :cut-button])
+                 (c/make :button "x1" [:id :normal-speed-button])
+                 (c/make :button "x0.5" [:id :half-speed-button])
+                 (c/make :panel [:id :timeline-area]) :span))
+
+(defn make-player-frame [image]
+  (clarity.utils/show-comp (make-player-panel image)))
+
+#_(clarity.utils/show-comp (make-player-panel))
--- src/film_tempo/vision.clj
+++ src/film_tempo/vision.clj
@@ -1,27 +1,28 @@
 (ns film-tempo.vision
   (:use vision.core)
-  (:require [film-tempo.video :as video]))
+  (:require [film-tempo.video :as video]
+            [film-tempo.gui :as gui]
+            [clarity.component :as c]))
 
 ;;"/Users/sideris/Movies/some.like.it.hot.avi"
 ;;"/Users/sideris/Downloads/StayingInLane_MPEG4.avi"
 
 (let [filename "/Users/sideris/Movies/some.like.it.hot.avi"
+      *image* (atom nil)
       capture (capture-from-file filename)
       frame-count (get-capture-property capture :frame-count)
-      width (get-capture-property capture :frame-width)
-      height (get-capture-property capture :frame-height)
-      fps (get-capture-property capture :fps)]
+      fps (get-capture-property capture :fps)
+      frame-delay (video/frame-delay fps)
+      player (gui/make-player-frame *image*)]
 
   (print "frame count:" frame-count "\n"
-         "w:" width "\n"
-         "h:" height "\n"
          "fps:" fps "\n"
          "duration:" (video/format-time (video/frame-to-time frame-count fps)) "\n")
 
-  #_(dotimes [_ frame-count]
+  (dotimes [_ frame-count]
     (let [frame (query-frame capture)]
-      ;;(print (.getHeight (force (:buffered-image frame))))
-      (view :raw frame)
-      (Thread/sleep 10)))
-  #_(release capture)
+      (swap! *image* (fn [_] (deref (:buffered-image frame))))
+      ;;(view :raw frame)
+      (Thread/sleep frame-delay)))
+  (release capture)
   (print "Done\n"))
